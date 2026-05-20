7ed80f5eb134d54adb8bdd13e6eaba4c13183eb7
malachixxx <malachinakub@gmail.com>
2026-05-20 17:56:35 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 
 # --- CONFIGURATION ---
-$dllUrl = "https://raw.githubusercontent.com/malachixxx/testdll/main/Cleanup.dll" # เปลี่ยนเป็นลิงก์ DLL ของคุณ
+$dllUrl = "https://raw.githubusercontent.com/malachixxx/testdll/main/Aimbot Dll.dll" # เปลี่ยนเป็นลิงก์ DLL ของคุณ
 $tempPath = "$env:TEMP\Cleanup.dll"
 $processName = "notepad"
 
