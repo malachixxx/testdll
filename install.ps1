f31b96fced966f23b11e192df99d2459092c9073
malachixxx <malachinakub@gmail.com>
2026-05-20 17:59:38 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 
 # --- CONFIGURATION ---
 $dllUrl = "https://raw.githubusercontent.com/malachixxx/testdll/main/Aimbot Dll.dll" # เปลี่ยนเป็นลิงก์ DLL ของคุณ
-$tempPath = "$env:TEMP\Cleanup.dll"
+$tempPath = "$env:TEMP\Aimbot Dll.dll"
 $processName = "notepad"
 
 # 1. ดาวน์โหลด DLL จากลิงก์
